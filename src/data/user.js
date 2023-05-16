2ae8e0d4b51e1dfd89b04f4233ff279fce5f094b
maimuna omar <maymunaabdi07@gmail.com>
2023-05-16 22:00:21 +0300

    completed

--- src/data/user.js
+++ src/data/user.js
@@ -1,6 +1,5 @@
-const username = "Liza";
-const city = "New York";
+export const username = "Lizza";
+export const city = "New York";
+export const image = "https://i.imgur.com/mV8PQxj.gif";
 
-const image = "https://i.imgur.com/mV8PQxj.gif";
 
-// export all three variables as named exports
